786baef0b82684307822619cb1eac5abe9f3b94d
QiuFeng54321 <32996262+QiuFeng54321@users.noreply.github.com>
2022-09-08 15:00:37 +0800

    Remove version (moved to assembly)
    Signed-off-by: QiuFeng54321 <32996262+QiuFeng54321@users.noreply.github.com>

--- PseudoCode.Core/Runtime/PseudoProgram.cs
+++ PseudoCode.Core/Runtime/PseudoProgram.cs
@@ -12,8 +12,6 @@ namespace PseudoCode.Core.Runtime;
 /// </summary>
 public class PseudoProgram
 {
-    public static readonly Version Version = new(1, 3, 3);
-
     /// <summary>
     ///     Stores feedbacks during parsing and typechecking (metaoperation)
     /// </summary>
