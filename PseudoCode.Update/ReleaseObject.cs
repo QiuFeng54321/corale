5739e3be7817c82106b163397b5ac7fcd80fa02f
QiuFeng54321 <32996262+QiuFeng54321@users.noreply.github.com>
2023-08-21 10:44:03 +0800

    Revert .net version
    Signed-off-by: QiuFeng54321 <32996262+QiuFeng54321@users.noreply.github.com>

--- PseudoCode.Update/ReleaseObject.cs
+++ PseudoCode.Update/ReleaseObject.cs
@@ -1,12 +1,25 @@
+using Newtonsoft.Json;
+
 namespace PseudoCode.Update;
 
 public class Asset
 {
+    [JsonConstructor]
+    public Asset()
+    {
+    }
+
     public string BrowserDownloadUrl { get; set; }
     public string Name { get; set; }
 }
+
 public class ReleaseObject
 {
+    [JsonConstructor]
+    public ReleaseObject()
+    {
+    }
+
     public string TagName { get; set; }
     public string Name { get; set; }
     public List<Asset> Assets { get; set; }
